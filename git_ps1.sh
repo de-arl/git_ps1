68e9caaf35e0a03cd1e2708d7fd62409aff1cdbc
Andreas Laible <a.laible@tum.de>
2024-02-22 20:59:55 +0100

    Small change, such that color is reseted to default after prompting the git branch.

--- git_ps1.sh
+++ git_ps1.sh
@@ -1,3 +1,5 @@
+### PUT THIS CODE IN ~/.bashrc ###
+
 # Function returns the current branch name as string
 #  if the working directory is under git version control
 git_ps1()
@@ -7,4 +9,4 @@ git_ps1()
 }
 
 # Use git_ps1 function in PS1 to prepend git working branch to bash prompt.
-PS1="\[\033[01;31m\]\$(git_ps1)${PS1}"
+PS1="\[\033[01;31m\]\$(git_ps1)\[e[0m\]${PS1}"
